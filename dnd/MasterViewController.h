7b3d1c93cfb2bc145ad9a4da416167dd168dd0f5
Devon Tivona <devon@tivona.me>
2016-11-18 21:06:02 -0700

    Initial Commit

---
+++ dnd/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  dnd
+//
+//  Created by Devon Tivona on 11/18/16.
+//  Copyright © 2016 Tivona & Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
